bde1578758405eed35900b6d9c1582879d0b09d6
Filip Razek <filip.razek@protonmail.com>
2026-07-26 15:07:49 +0200

    feat: Create working Anki login

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,74 @@
+use std::println;
+
+use serde::{Deserialize, Serialize};
+use zstd::{Decoder, stream::write::Encoder};
+
 fn main() {
-    println!("Hello, world!");
+    trpl::block_on(login());
+}
+
+#[derive(Serialize)]
+struct LoginAnkiSyncHeader {
+    v: i32,
+    k: String,
+    c: String,
+    s: String
+}
+
+#[derive(Serialize)]
+struct LoginBody {
+    u: String,
+    p: String
+}
+
+#[derive(Debug, Deserialize)]
+struct LoginResult {
+    key: String,
+} 
+
+async fn login() {
+
+    /*
+    POST https://sync.ankiweb.net/sync/hostKey
+        Content-Type: application/octet-stream
+        anki-sync: {"v":11,"k":"","c":"anki,2.x.x","s":"<any-session-id>"}
+        <body = zstd( {"u":"<user>","p":"<pass>"} )>
+
+     */
+    let client_version = format!(
+            "{app},{version}",
+            app = "anki-dl",
+            version = "0.1.0"
+        );
+
+    let login_header = LoginAnkiSyncHeader {
+        v: 11,
+        c: client_version,
+        k: String::from(""),
+        s: String::from("test_session")
+    };
+    let body = LoginBody {
+        u: String::from("filip@razek.org"),
+        p: String::from("PASSWORD"),
+    };
+
+    let mut encoder = Encoder::new(Vec::new(), 0).unwrap();
+    serde_json::to_writer(&mut encoder, &body).unwrap();
+    let result = encoder.finish().unwrap();
+
+
+    let client = reqwest::Client::new();
+
+    let res = client.post("https://sync.ankiweb.net/sync/hostKey")
+        .header("anki-sync", serde_json::to_string(&login_header).unwrap())
+        .header("Content-Type", "application/octet-stream")
+        .body(result)
+        .send()
+        .await
+        .unwrap();
+
+    let bytes = res.bytes().await.unwrap();
+    let decoder = Decoder::new(&bytes[..]).unwrap();
+    let data: LoginResult = serde_json::from_reader(decoder).unwrap();
+    println!("{:?}", data);
 }
