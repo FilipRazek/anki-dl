2f6ee4d22e47e18cf8c3a331fc180b8077a3419f
Filip Razek <filip.razek@protonmail.com>
2026-07-27 16:02:46 +0200

    refactor: Extract logic to send_login submodule

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::println;
 use std::env;
-use decompress::decompress_result;
 use send_login_request::send_login_request;
 use dotenv::dotenv;
 
-mod decompress;
 mod send_login_request;
 
 fn main() {
@@ -12,16 +10,7 @@ fn main() {
 }
 
 async fn login() {
-    /*
-    POST https://sync.ankiweb.net/sync/hostKey
-        Content-Type: application/octet-stream
-        anki-sync: {"v":11,"k":"","c":"anki,2.x.x","s":"<any-session-id>"}
-        <body = zstd( {"u":"<user>","p":"<pass>"} )>
-
-     */
-    let res = send_login_request(load_password()).await.unwrap();
-    let bytes = res.bytes().await.unwrap();
-    let data = decompress_result(&bytes[..]).unwrap();
+    let data = send_login_request(load_password()).await.unwrap();
     println!("{}", data.key);
 }
 
--- src/send_login_request/mod.rs
+++ src/send_login_request/mod.rs
@@ -1,10 +1,23 @@
 
+use std::io::Error;
+
 use login_request_body::{build_anki_sync_header, build_request_body};
 use reqwest::Response;
+use decompress::decompress_result;
 
+use crate::send_login_request::decompress::LoginResult;
+
+mod decompress;
 mod login_request_body;
 
-pub fn send_login_request(password: String) -> impl Future<Output = Result<Response, reqwest::Error>> {
+pub async fn send_login_request(password: String) -> Result<LoginResult, Error> {
+    let result = inner_send(password).await.unwrap().bytes();
+    let bytes = result.await.unwrap();
+    decompress_result(&bytes[..])
+}
+
+
+fn inner_send(password: String) -> impl Future<Output = Result<Response, reqwest::Error>> {
     let body = build_request_body(password);
     let anki_sync_header = build_anki_sync_header();
     let client = reqwest::Client::new();
