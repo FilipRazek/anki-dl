28144930ae220431681c0301c828c8bc2aa61364
Filip Razek <filip.razek@protonmail.com>
2026-07-28 09:44:10 +0200

    refactor: clean submodule

--- src/send_login_request/mod.rs
+++ src/send_login_request/mod.rs
@@ -1,25 +1,23 @@
 
 use std::io::Error;
-
-use login_request_body::{build_anki_sync_header, build_request_body};
 use reqwest::Response;
 use decompress::decompress_result;
 
 use crate::send_login_request::decompress::LoginResult;
 
 mod decompress;
-mod login_request_body;
+mod request_body;
+mod request_headers;
 
 pub async fn send_login_request(password: String) -> Result<LoginResult, Error> {
-    let result = inner_send(password).await.unwrap().bytes();
+    let result = send_http_request(password).await.unwrap().bytes();
     let bytes = result.await.unwrap();
     decompress_result(&bytes[..])
 }
 
-
-fn inner_send(password: String) -> impl Future<Output = Result<Response, reqwest::Error>> {
-    let body = build_request_body(password);
-    let anki_sync_header = build_anki_sync_header();
+fn send_http_request(password: String) -> impl Future<Output = Result<Response, reqwest::Error>> {
+    let body = request_body::build(password);
+    let anki_sync_header = request_headers::build_anki_sync();
     let client = reqwest::Client::new();
     client.post("https://sync.ankiweb.net/sync/hostKey")
         .header("anki-sync", anki_sync_header)
---
+++ src/send_login_request/request_headers.rs
@@ -0,0 +1,25 @@
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct LoginAnkiSyncHeader {
+    v: i32,
+    k: String,
+    c: String,
+    s: String
+}
+
+pub fn build_anki_sync() -> String {
+    let client_version = format!(
+            "{app},{version}",
+            app = "anki-dl",
+            version = "0.1.0"
+        );
+
+    let login_header = LoginAnkiSyncHeader {
+        v: 11,
+        c: client_version,
+        k: String::from(""),
+        s: String::from("test_session")
+    };
+    serde_json::to_string(&login_header).unwrap()
+}
